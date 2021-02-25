a7b9302dc4b68da26773fddcc85121e3bf1e181f
Rifqirz <79630539+Rifqirz@users.noreply.github.com>
2021-02-25 16:40:29 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -17,12 +17,17 @@ namespace TROLLEY
             InitializeComponent();
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
